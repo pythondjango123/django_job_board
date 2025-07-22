5821dd4b273e67831175431a746ba32fd6235628
saiprasanna <vasamsaiprasanna@gmail.com>
2025-07-22 12:44:56 +0530

    Add collected static files for deployment

--- job_board_project/settings.py
+++ job_board_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-at4!jgs@8g)yp^&e__=6k)8_4=z=up%m7ac=e3b=%oq=za8xmj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['studentsdjango.pythonanywhere.com']
 
 
 # Application definition
@@ -131,3 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'home'  
 LOGOUT_REDIRECT_URL = 'logout_confirm'
 LOGIN_URL = 'login'
+STATIC_ROOT = BASE_DIR / 'static'
